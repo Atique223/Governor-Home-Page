b68d55b4b2704f7257016ba9ec5dd37e32dcbaf2
Atique223 <159752093+Atique223@users.noreply.github.com>
2025-01-19 23:18:58 +0500

    Update react-slick.d.ts

--- src/types/react-slick.d.ts
+++ src/types/react-slick.d.ts
@@ -1,4 +1,11 @@
+// declare module "react-slick" {
+//     const Slider: any;
+//     export default Slider;
+// }
+import { ComponentType } from "react";
+
 declare module "react-slick" {
-    const Slider: any;
-    export default Slider;
+  const Slider: ComponentType<any>; // Use ComponentType to define React components
+  export default Slider;
 }
+
